03444a5079d1380133b7b2941cdda7e1f82c493d
ThongDang251999 <minhthong251999@gmail.com>
2025-05-04 14:32:46 -0600

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,14 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { CssBaseline } from '@mui/material';
 import axios from 'axios';
 
 // Set base URL for axios
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://final-project-1-22hm.onrender.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssBaseline />
     <App />
   </React.StrictMode>
 ); 
