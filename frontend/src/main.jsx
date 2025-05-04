5eab1696f0be922ed8ad63e2f8be0077ee72b22d
ThongDang251999 <minhthong251999@gmail.com>
2025-05-04 14:43:28 -0600

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import axios from 'axios';
+import { PreferenceProvider } from './context/PreferenceContext';
+import { AuthProvider } from './context/AuthContext';
 
 // Set base URL for axios
 axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://final-project-1-22hm.onrender.com';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <PreferenceProvider>
+        <App />
+      </PreferenceProvider>
+    </AuthProvider>
   </React.StrictMode>
 ); 
